3756893d37f9492522c10e92d5dd3a1690234c97
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-06-26 10:33:47 -0700

    User Registration

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -14,3 +14,33 @@ def user_login():
 @app.route("/user/register")
 def user_register():
     return render_template("user/userRegister.html")
+
+@app.route("/user_register", methods= ['POST'])
+def register_user():
+    if User.validate_register(request.form) == True:
+        data = {
+            "email" : request.form['email']
+        }
+
+        result = User.get_one(data)
+        if result == None:
+            data = {
+                "title" : request.form['title'],
+                "first_name" : request.form['first_name'],
+                "last_name" : request.form['last_name'],
+                "email" : request.form['email'],
+                "password" : request.form['password'],
+                "pin" : request.form['pin']
+            }
+            admin_id = Administrator.add_one(data)
+            session['admin_id'] = admin_id
+            session['first_name'] = request.form['first_name']
+            session['last_name'] = request.form['last_name']
+            session['email'] = request.form['email']
+            session['pin'] = request.form['pin']
+            return redirect("/admin/dashboard")
+        else:
+            flash("Email already in use, please provide another", "error_register_email")
+            return redirect("/admin")
+    else:
+        return redirect("/user/register")
--- flask_app/models/user_model.py
+++ flask_app/models/user_model.py
@@ -19,3 +19,61 @@ class User:
         self.accounts = []
         self.loans = []
         self.cards = []
+
+    @staticmethod
+    def validate_register(data):
+        isValid = True
+        if data['first_name'] == "":
+            flash("You must provide your first name.", "error_register_first_name" )
+            isValid = False
+        if len(data['first_name']) < 3:
+            flash("Your first name must have at least 3 characters.", "error_register_first_name" )
+            isValid = False
+        if data['last_name'] == "":
+            flash("You must provide your last name.", "error_register_last_name" )
+            isValid = False
+        if len(data['last_name']) < 3:
+            flash("Your last name must have at least 3 characters.", "error_register_last_name" )
+            isValid = False
+        if data['email'] == "":
+            flash("You must provide your email.", "error_register_email" )
+            isValid = False
+        if not EMAIL_REGEX.match(data['email']):
+            flash("Please provide a valid email.", "error_register_email")
+            isValid = False
+        if data['date_of_birth'] == "":
+            flash("You must provide your date of birth.", "error_register_birth" )
+            isValid = False
+        if len(data['social_security']) < 9:
+            flash("Invalid Social Security", "error_register_social")
+            isValid = False
+        if data['employment_status'] == "Choose...":
+            flash("You must provide your employment status.", "error_register_employment" )
+            isValid = False
+        if data['street'] == "":
+            flash("You must provide your Street.", "error_register_street" )
+            isValid = False
+        if data['city'] == "":
+            flash("You must provide your City.", "error_register_city" )
+            isValid = False
+        if data['state'] == "Choose...":
+            flash("You must provide your State.", "error_register_state" )
+            isValid = False
+        if len(data['zip']) < 5:
+            flash("Invalid Zipcode", "error_register_zip")
+            isValid = False
+        if data['username'] == "":
+            flash("You must provide a username.", "error_register_username")
+            isValid = False
+        if len(data['username']) < 5:
+            flash("Username must be at least 5 characters long.", "error_register_username")
+            isValid = False
+        if data['password'] == "":
+            flash("You must provide a password.", "error_register_password")
+            isValid = False
+        if len(data['password']) < 8:
+            flash("Password must be at least 8 characters long.", "error_register_password")
+            isValid = False
+        if data['password_confirmation'] != data['password']:
+            flash("Your password confirmationan doesn't match.", "error_register_password_confirmation")
+        return isValid
