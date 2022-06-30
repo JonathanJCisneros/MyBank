8eed4031ca82287ee16f5536483c91832281558b
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-06-29 21:36:03 -0700

    Fixed class attributes

--- flask_app/models/address_model.py
+++ flask_app/models/address_model.py
@@ -10,6 +10,8 @@ class Address:
         self.city = data['city']
         self.state = data['state']
         self.zipcode = data['zipcode']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
         self.users_id = data['users_id']
 
     @classmethod
