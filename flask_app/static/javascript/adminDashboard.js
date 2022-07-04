998e5f31de64bf27764fce77344f4fc31550ffe4
Jonathan Cisneros <jonathan.james.cisneros@outlook.com>
2022-07-03 18:53:49 -0700

    working on comment details/ admin

---
+++ flask_app/static/javascript/adminDashboard.js
@@ -0,0 +1,7 @@
+function openTheForm() {
+    document.getElementById("comments").style.display = "block";
+}
+
+function closeTheForm() {
+    document.getElementById("comments").style.display = "none";
+}
